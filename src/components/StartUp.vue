f3a4bd9219aeec852802703dad4b7eff7e9130e3
uchedotphp <uchedotphp@gmail.com>
2020-09-10 05:23:00 +0100

    removed vuejs default templates and created a StartUp.vue component in the components folder

---
+++ src/components/StartUp.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    front page
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StartUpComponent",
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <StartUp />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import StartUp from "@/components/StartUp";
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    StartUp,
+  },
 };
 </script>
