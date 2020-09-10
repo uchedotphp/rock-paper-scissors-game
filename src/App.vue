51226fe37577fae41313c0cd2791c71fbb573482
uchedotphp <uchedotphp@gmail.com>
2020-09-10 04:49:02 +0100

    importing style guide into tailwindcss.config.js file: colors, backgrounds

--- src/App.vue
+++ src/App.vue
@@ -19,18 +19,20 @@ import HelloWorld from "./components/HelloWorld.vue";
 export default {
   name: "app",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@600;700&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.app {
+  font-family: "Barlow Semi Condensed", sans-serif;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary Colors
+        scissorsGradient: "hsl(39, 89 %, 49 %) to hsl(40, 84%, 53%)",
+        paperGradient: "hsl(230, 89%, 62%) to hsl(230, 89%, 65%)",
+        rockGradient: "hsl(349, 71 %, 52 %) to hsl(349, 70%, 56%)",
+        lizardGradient: "hsl(261, 73 %, 60 %) to hsl(261, 72%, 63%)",
+        cyan: "hsl(189, 59 %, 53 %) to hsl(189, 58%, 57%)",
+
+        // Neutral Colors
+        darkText: "hsl(229, 25 %, 31 %)",
+        scoreText: "hsl(229, 64 %, 46 %)",
+        headerOutline: "hsl(217, 16 %, 45 %)",
+
+        // Background Colors
+        radialGradient: "hsl(214, 47 %, 23 %) to hsl(237, 49%, 15%)",
+      },
+    },
   },
   variants: {},
   plugins: [],
