2d1a60655e35680d037441d2969b836350cd3488
uchedotphp <uchedotphp@gmail.com>
2020-09-11 06:22:54 +0100

    built RULES button

--- src/components/StartUp.vue
+++ src/components/StartUp.vue
@@ -2,12 +2,20 @@
   <div class="container mx-auto">
     <div class="px-6 h-screen text-white grid grid-cols-1 content-center">
       <ScoreCard />
-      <p class="bg-black">
+      <p class="bg-black my-24">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione
         distinctio consectetur aperiam quas ipsa similique doloribus corrupti,
         explicabo itaque minima fuga cumque harum adipisci sed laboriosam
         repellat impedit, quod aliquid.
       </p>
+
+      <div class="mx-auto">
+        <button
+          class="bg-transparent border-2 border-gray-500 px-8 py-2 rounded-md uppercase"
+        >
+          rules
+        </button>
+      </div>
     </div>
   </div>
 </template>
