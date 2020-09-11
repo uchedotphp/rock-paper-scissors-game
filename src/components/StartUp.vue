3f74502f24fa1c19cb304903e5b95d75868efa83
uchedotphp <uchedotphp@gmail.com>
2020-09-11 04:45:26 +0100

    edited the colors onject in tailwind.config.js file in order to fit into the designs for use as css gradients

--- src/components/StartUp.vue
+++ src/components/StartUp.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    front page
+  <div class="container h-screen bg-radialGradientOne">
+    <div class="text-red-600 grid grid-cols-2">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione
+        distinctio consectetur aperiam quas ipsa similique doloribus corrupti,
+        explicabo itaque minima fuga cumque harum adipisci sed laboriosam
+        repellat impedit, quod aliquid.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione
+        distinctio consectetur aperiam quas ipsa similique doloribus corrupti,
+        explicabo itaque minima fuga cumque harum adipisci sed laboriosam
+        repellat impedit, quod aliquid.
+      </p>
+    </div>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,25 @@ module.exports = {
     extend: {
       colors: {
         // Primary Colors
-        scissorsGradient: "hsl(39, 89 %, 49 %) to hsl(40, 84%, 53%)",
-        paperGradient: "hsl(230, 89%, 62%) to hsl(230, 89%, 65%)",
-        rockGradient: "hsl(349, 71 %, 52 %) to hsl(349, 70%, 56%)",
-        lizardGradient: "hsl(261, 73 %, 60 %) to hsl(261, 72%, 63%)",
-        cyan: "hsl(189, 59 %, 53 %) to hsl(189, 58%, 57%)",
+        scissorsGradientOne: "hsl(39, 89%, 49%)",
+        scissorsGradientTwo: "hsl(40, 84%, 53%)",
+        paperGradient: "hsl(230, 89%, 62%)",
+        paperGradientTwo: "hsl(230, 89%, 65%)",
+        rockGradient: "hsl(349, 71%, 52%)",
+        rockGradientTwo: "hsl(349, 70%, 56%)",
+        lizardGradient: "hsl(261, 73%, 60%)",
+        lizardGradientTwo: "hsl(261, 72%, 63%)",
+        cyanOne: "hsl(189, 59%, 53%)",
+        cyanTwo: "hsl(189, 58%, 57%)",
 
         // Neutral Colors
-        darkText: "hsl(229, 25 %, 31 %)",
-        scoreText: "hsl(229, 64 %, 46 %)",
-        headerOutline: "hsl(217, 16 %, 45 %)",
+        darkText: "hsl(229, 25%, 31%)",
+        scoreText: "hsl(229, 64%, 46%)",
+        headerOutline: "hsl(217, 16%, 45%)",
 
         // Background Colors
-        radialGradient: "hsl(214, 47 %, 23 %) to hsl(237, 49%, 15%)",
+        radialGradientOne: "hsl(214, 47%, 23%)",
+        radialGradientTwo: "hsl(237, 49%, 15%)",
       },
     },
   },
