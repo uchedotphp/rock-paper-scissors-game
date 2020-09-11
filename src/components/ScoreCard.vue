40625705e194cf3bbf2c8e5e1d58724cf288d474
uchedotphp <uchedotphp@gmail.com>
2020-09-11 06:07:21 +0100

    did more customization on the rock, paper, scissors text by reducing the line height and also did the same for the score number 12

--- src/components/ScoreCard.vue
+++ src/components/ScoreCard.vue
@@ -2,18 +2,18 @@
   <div
     class="border-4 rounded-lg border-headerOutline p-5 grid grid-cols-2 uppercase"
   >
-    <p class="flex items-center font-bold text-2xl leading-none">
+    <p class="flex items-center font-bold text-xl leading-4">
       rock <br />
       paper <br />
       scissors
     </p>
     <div
-      class="bg-white rounded-md justify-self-end text-center px-8 py-2 uppercase"
+      class="bg-white rounded-md justify-self-end text-center px-8 py-3 uppercase"
     >
       <p class="text-scoreText text-sm font-semibold">
         score
       </p>
-      <p class="text-darkText text-4xl font-bold">
+      <p class="text-darkText text-5xl font-bold leading-none">
         12
       </p>
     </div>
