cf2c8c4086562fcca1486fef15c4cb3a549c9423
uchedotphp <uchedotphp@gmail.com>
2020-09-11 05:52:57 +0100

    created a ScoreCard.vue component and moved the finished top design from StartUp.vue into ScoreCard.vue

---
+++ src/components/ScoreCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="border-4 rounded-lg border-headerOutline p-5 grid grid-cols-2 uppercase"
+  >
+    <p class="flex items-center">
+      rock <br />
+      paper <br />
+      scissors
+    </p>
+    <div
+      class="bg-white rounded-md justify-self-end text-center px-8 py-2 uppercase"
+    >
+      <p class="text-scoreText text-sm font-semibold">
+        score
+      </p>
+      <p class="text-darkText text-4xl font-bold">
+        12
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ScoreCardComponent",
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/StartUp.vue
+++ src/components/StartUp.vue
@@ -1,26 +1,7 @@
 <template>
   <div class="container mx-auto">
     <div class="px-6 h-screen text-white grid grid-cols-1 content-center">
-      <div
-        class="border-4 rounded-lg border-headerOutline p-5 grid grid-cols-2 uppercase"
-      >
-        <p class="flex items-center">
-          rock <br />
-          paper <br />
-          scissors
-        </p>
-        <div
-          class="bg-white rounded-md justify-self-end text-center px-8 py-2 uppercase"
-        >
-          <p class="text-scoreText text-sm font-semibold">
-            score
-          </p>
-          <p class="text-darkText text-4xl font-bold">
-            12
-          </p>
-        </div>
-      </div>
-
+      <ScoreCard />
       <p class="bg-black">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione
         distinctio consectetur aperiam quas ipsa similique doloribus corrupti,
@@ -32,8 +13,12 @@
 </template>
 
 <script>
+import ScoreCard from "@/components/ScoreCard";
 export default {
   name: "StartUpComponent",
+  components: {
+    ScoreCard,
+  },
 };
 </script>
 
