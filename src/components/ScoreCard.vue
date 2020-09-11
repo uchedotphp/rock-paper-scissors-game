0b968f96fdd7ab897a0c77f4d9f6f68ca4c5e9e4
uchedotphp <uchedotphp@gmail.com>
2020-09-11 09:54:30 +0100

    customized the ScoreCard for better views of devices of tablet and higher

--- src/components/ScoreCard.vue
+++ src/components/ScoreCard.vue
@@ -1,19 +1,21 @@
 <template>
   <div
-    class="border-4 rounded-lg border-headerOutline pl-5 pr-3 py-5 grid grid-cols-2 uppercase"
+    class="border-4 rounded-lg border-headerOutline pl-5 pr-3 py-5 md:pl-8 md:pr-6 grid grid-cols-2 uppercase"
   >
-    <p class="flex items-center font-bold text-xl leading-4">
+    <p
+      class="flex items-center font-bold text-xl md:text-5xl md:leading-none leading-4"
+    >
       rock <br />
       paper <br />
       scissors
     </p>
     <div
-      class="bg-white rounded-md justify-self-end text-center px-8 py-3 uppercase"
+      class="bg-white rounded-md justify-self-end text-center px-8 py-4 md:px-12 uppercase"
     >
-      <p class="text-scoreText text-sm font-semibold">
+      <p class="text-scoreText text-sm md:text-lg font-semibold">
         score
       </p>
-      <p class="text-darkText text-5xl font-bold leading-none">
+      <p class="text-darkText text-5xl font-bold leading-none md:text-6xl">
         12
       </p>
     </div>
